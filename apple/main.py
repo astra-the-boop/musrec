b25a0332e2246e505882fc38ec6fe77131a77184
astra-the-boop <astra.the.boop@icloud.com>
2025-06-23 06:35:51 +0700

    added ... a lot :3c

--- apple/main.py
+++ apple/main.py
@@ -1,40 +1,104 @@
 #requires Blackhole installed and set up with 2 channels because tim apple is a bitch
 #https://github.com/ExistentialAudio/BlackHole
+#also ffmpeg too
+
 
 import tracks as t
 import sounddevice as sd
 import soundfile as sf
-
-t.pause()
-duration = t.getDuration()
-if t.getPosition() != 0:
-    t.setPlayerPos(0)
-sample_rate = 44100
-channels = 2
-
-print(sd.query_devices())
-
+import time, os, subprocess
+import numpy as np
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC, error
 
 device_index = next(
     (i for i, d in enumerate(sd.query_devices()) if "BlackHole" in d["name"]), None
-) #Checks if blackhole is installed on device
+)  # Checks if blackhole is installed on device
 
 if device_index is None:
     raise RuntimeError("""It seems like BlackHole isn't installed :( Please install it and try again.
-    
-    https://github.com/ExistentialAudio/BlackHole
-    
-    If it is installed then check if the name of the audio device is 'BlackHole 2ch' in Audio MIDI Setup
-    
-    If you're not on a Mac device, please use the other version""")
 
+    https://github.com/ExistentialAudio/BlackHole
+
+    If it is installed then check if the name of the audio device is 'BlackHole 2ch' in Audio MIDI Setup
+
+    If you're not on a Mac device, please use the other version""")
 
 print(f"Using device index: {device_index}")
 
-audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels, dtype='float32', device=device_index)
-t.play()
+
+sample_rate = 44100
+channels = 2
+blocksize = 1024
+
+print(sd.query_devices())
+
+fileType = input("Enter which file type you want exported to (default: .wav) (supported: .wav (no metadata), .mp3)_ ")
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status)
+    recordedChunks.append(indata.copy())
 
 
-sd.wait()
+for i in range(int(input("Amount of tracks to record_"))):
+    interrupted = False
+    t.pause()
+    duration = t.getDuration()
+    if t.getPosition() != 0:
+        t.setPlayerPos(0)
+    title = t.getTitle()
+    artist = t.getArtist()
+    album = t.getAlbum()
+    recordedChunks = []
+    t.play()
+    print(f"Currently recording {t.getTitle()} by {t.getArtist()} with length of {t.getDuration()} seconds; Pause music to stop recording")
+    with sd.InputStream(
+            samplerate=sample_rate,
+            channels=channels,
+            callback=callback,
+            blocksize=blocksize,
+        device=device_index,
+        dtype='float32'
+    ):
+        start_time = time.time()
+        time.sleep(1)
+        while t.isPlaying() and (time.time() - start_time) < duration:
+            time.sleep(0.1)
+        print("Recording stopped")
+    interrupted = t.isPlaying() == False and (time.time() - start_time) < duration
 
-sf.write("output.wav", audio, sample_rate)
+    audio = np.concatenate(recordedChunks, axis=0)
+    if not interrupted:
+        sf.write(f"{title} — {artist}.wav", audio, sample_rate)
+        if fileType == "wav":
+            print(f"Saved as '{title} — {artist}.wav'. If audio is blank, check if 'BlackHole 2ch' or a multi-output device with it is being used for sound output in Audio MIDI Setup")
+        else:
+            subprocess.run(["ffmpeg", "-y", "-i", f"{title} — {artist}.wav", "-codec:a", "libmp3lame", "-qscale:a", "0", f"{title} — {artist}.mp3"])
+            os.remove(f"{title} — {artist}.wav")
+            print(f"Saved as '{title} — {artist}.mp3'. If audio is blank, check if 'BlackHole 2ch' or a multi-output device with it is being used for sound output in Audio MIDI Setup")
+            print("Writing metadata...")
+            file = EasyID3(f"{title} — {artist}.mp3")
+            file["title"] = title
+            file["artist"] = artist
+            file["album"] = album
+            file.save()
+            file = ID3(f"{title} — {artist}.mp3")
+            if t.fetchAlbumCover(title, artist, album, "cover.jpg") != None:
+                with open("cover.jpg", "rb") as albumArt:
+                    file.add(APIC(
+                        encoding=3,
+                        mime='image/jpeg',
+                        type=3,
+                        desc=f"Cover of {title} — {artist}",
+                        data=albumArt.read()
+                    ))
+                file.save()
+            print("Metadata saved")
+            try:
+                os.remove("cover.jpg")
+            except FileNotFoundError:
+                pass
+
+    else:
+        print("Recording not saved due to user interrupt")
--- apple/tracks.py
+++ apple/tracks.py
@@ -1,16 +1,16 @@
 import subprocess
+import requests
 
 def getDuration():
-    results = subprocess.check_output([
+    return float(subprocess.check_output([
         "osascript", "-e", "tell application \"Spotify\" to get duration of current track"
-    ])
-    return float(results.strip())/1000
+    ]).strip())/1000
 
 def getPosition():
-    results = subprocess.check_output([
+    return float(subprocess.check_output([
         "osascript", "-e", "tell application \"Spotify\" to get player position"
-    ])
-    return float(results.strip())
+    ]).strip())
+
 
 def setPlayerPos(position):
     subprocess.run([
@@ -26,3 +26,44 @@ def play():
     subprocess.run([
         "osascript", "-e", "tell application \"Spotify\" to play"
     ])
+
+def getTitle():
+    try:
+        return subprocess.check_output([
+            "osascript", "-e", "tell application \"Spotify\" to get name of current track"
+        ]).decode().strip()
+    except:
+        return None
+
+def getArtist():
+    try:
+        return subprocess.check_output([
+            "osascript", "-e", "tell application \"Spotify\" to get artist of current track"
+        ]).decode().strip()
+    except:
+        return None
+
+def getAlbum():
+    try:
+        return subprocess.check_output(["osascript", "-e", "tell application \"Spotify\"\nalbum of current track\nend tell"], text=True).strip()
+    except subprocess.CalledProcessError:
+        return None
+
+def isPlaying():
+    try:
+        return True if subprocess.check_output(["osascript", "-e", "tell application \"Spotify\" to get player state"]).decode().strip() == "playing" else False
+    except subprocess.CalledProcessError:
+        return False
+
+def fetchAlbumCover(title, artist, album, saveAs="cover.jpg"):
+    query = f"{title} {artist} {album}".replace(" ", "+")
+    response = requests.get(f"https://itunes.apple.com/search?term={query}&limit=1&media=music")
+    data = response.json()
+    if data["resultCount"] == 0:
+        print("No cover found")
+        return None
+    artworkUrl = data["results"][0]["artworkUrl100"].replace("100x100", "600x600")
+    imgData = requests.get(artworkUrl).content
+    with open(saveAs, "wb") as f:
+        f.write(imgData)
+    return saveAs
